1464362d2f792d57cea01082abc04b4b6b054e7e
Ashwin1234 <ashwinsrivatsab@gmail.com>
2021-07-17 02:21:55 -0500

    CRUD on receiver and updated schema and queries

--- covid_vaccination/src/main/java/com/covid/vaccination/controller/UserController.java
+++ covid_vaccination/src/main/java/com/covid/vaccination/controller/UserController.java
@@ -14,11 +14,13 @@ public class UserController {
     @Autowired
     private UserService userservice;
 
+    //get all users
     @RequestMapping(value = "/user",method = RequestMethod.GET)
     public List<User> getAllUsers(){
         return userservice.getALlUsers();
     }
 
+    //get user given id
     @RequestMapping(value="/user/{id}",method=RequestMethod.GET)
     public User getUserById(@PathVariable String id){
 
@@ -26,17 +28,21 @@ public class UserController {
 
     }
 
+    //add user
     @RequestMapping(value="/user",method=RequestMethod.POST)
     public void addUser(@RequestBody User user){
         userservice.addUser(user);
 
     }
 
+    //update user given id
     @RequestMapping(value = "/user/{id}",method=RequestMethod.PUT)
     public void updateUser(@RequestBody User user,@PathVariable String id){
         userservice.updateUser(user,id);
 
     }
+
+    //delete user given id
     @RequestMapping(value="/user/{id}",method=RequestMethod.DELETE)
     public void deleteUser(@PathVariable String id){
         userservice.deleteUser(id);
--- covid_vaccination/src/main/java/com/covid/vaccination/controller/VaccineController.java
+++ covid_vaccination/src/main/java/com/covid/vaccination/controller/VaccineController.java
@@ -14,22 +14,26 @@ public class VaccineController {
 
 	@Autowired
 	VaccineService vaccineService;
-	
+
+	//get all vaccines
 	@RequestMapping(value = "/vaccine", method = RequestMethod.GET)
 	public List<Vaccine> getAllVaccines(){
 		return vaccineService.getAllVaccines();
 	}
 
+	//add vaccine
 	@RequestMapping(value="/vaccine",method = RequestMethod.POST)
 	public void addVaccine(@RequestBody Vaccine vaccine){
 		vaccineService.addVaccine(vaccine);
 	}
 
+	//update vaccine given id
 	@RequestMapping(value="/vaccine/{id}",method=RequestMethod.PUT)
 	public void updateVaccine(@RequestBody Vaccine vaccine,@PathVariable String id){
 		vaccineService.updateVaccine(vaccine,id);
 	}
 
+	//delete vaccine given id
 	@RequestMapping(value="/vaccine/{id}",method=RequestMethod.DELETE)
 	public void deleteVaccine(@PathVariable String id){
 		vaccineService.deleteVaccine(id);
